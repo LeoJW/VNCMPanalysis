f4bbb6f8b1cbe98314377d1125a4203247a1588e
Wood <lwood39@gatech.edu>
2025-04-24 15:11:10 -0400

    sorta can parallize
    progress on training in parallel. Doesn't retrieve data (easy), but needs to be switched to something like a pool (harder) because currently tries to spawn so many processes that eat up GPU memory

---
+++ eslam_MI/multiprocessing_test.py
@@ -0,0 +1,114 @@
+import os
+os.environ['CUDA_LAUNCH_BLOCKING'] = '1'
+
+import torch
+import warnings
+import time
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.multiprocessing as mp
+import torchvision.transforms.functional as TF
+import numpy as np
+import pandas as pd
+from scipy.ndimage import gaussian_filter1d
+import matplotlib.pyplot as plt
+import torch.optim as optim
+
+from torch.utils.data import Dataset, DataLoader
+
+warnings.filterwarnings("ignore")
+
+# Import MI files
+from utils import *
+from models import *
+from estimators import *
+from trainers import *
+
+# Intialize device
+if torch.cuda.is_available():
+    device = 'cuda'
+elif torch.backends.mps.is_available():
+    device = 'mps'
+else:
+    device = "CPU"
+print(f'Device: {device}')
+
+
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    
+    data_dir = os.path.join(os.getcwd(), 'data_for_python')
+    X, Y, x_labels, y_labels = process_spike_data(os.path.join(data_dir, '2025-03-12_1'), 0.1)
+    print(f"Neural Activity (X): {X.shape}")
+    print(f"Muscle Activity (Y): {Y.shape}") 
+    print("Neuron Labels:", x_labels)
+    print("Muscle Labels:", y_labels)
+
+    # Optimizer parameters (for training)
+    opt_params = {
+        'epochs': 100,
+        'batch_size': 128,
+        'learning_rate': 5e-4,
+        'n_trials': 2,
+        'patience': 50,
+        'min_delta': 0.001
+    }
+    # Critic parameters for DSIB or DVSIB (for the estimator), except embed_dim, which changes with training
+    critic_params = {
+        'Nx': X.shape[0],
+        'Ny': Y.shape[0],
+        'layers': 2,
+        'hidden_dim': 256,
+        'activation': 'leaky_relu',
+        'beta': 512,
+        'max_dz': 10, # max value for embed_dim that we search for
+        'estimator': 'infonce', # Estimator: infonce or smile_5. See esitmators.py for all options
+        'mode': 'sep' # Almost always we'll use separable
+    }
+
+    # Make dataset into tensors
+    X, Y = torch.tensor(X, dtype=torch.float32), torch.tensor(Y, dtype=torch.float32)
+    dataset = BatchedDataset(X, Y, opt_params['batch_size'])
+    full_dataset = create_train_test_eval(dataset, train_fraction=0.95, device=device)
+
+    torch.cuda.empty_cache()
+
+    # First do a vanilla run to get calibrated
+    print('-------------- Vanilla run -------------')
+    start = time.time()
+    this_critic_params = {**critic_params, 'embed_dim': 1} # Choose dz
+    mis, mis_test = train_model(DSIB, full_dataset, opt_params, this_critic_params)
+    print(time.time() - start)
+    # torch.cuda.empty_cache()
+
+    # input_queue = mp.Queue()
+    # results_queue = mp.Queue()
+    # # Package together iterator of run_trial arguments for every condition
+    # iter = []
+    # for dz in range(1, critic_params['max_dz']+1):
+    #     this_critic_params = {**critic_params, 'embed_dim': dz}
+    #     for j in range(opt_params['n_trials']):
+    #         iter.append((
+    #             j, DSIB, input_queue, results_queue, opt_params, this_critic_params, device
+    #         ))
+
+    # processes = []
+    # for i in iter:
+    #     # Set up each model process
+    #     p = mp.Process(target=run_trial, args=i)
+    #     p.start()
+    #     processes.append(p)
+    #     # Put input data into queue
+    #     input_queue.put(dataset)
+    
+    # time.sleep(1)
+    
+    # for p in processes:
+    #     p.join()
+
+    # # Collect results
+    # results = []
+    # while not results_queue.empty():
+    #     results.append(results_queue.get())
--- eslam_MI/trainers.py
+++ eslam_MI/trainers.py
@@ -4,6 +4,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
+from utils import *
+
 # Check if CUDA or MPS is running
 if torch.cuda.is_available():
     device = 'cuda'
@@ -13,7 +15,7 @@ else:
     device = "cpu"
 
 # Train function with early stopping, serial form
-def train_model(model, full_dataset, opt_params, model_type="dsib", device=device, patience=50, min_delta=0.001):
+def train_model(model_func, full_dataset, opt_params, critic_params, device=device):
     """
     Generalized training function for DSIB and DVSIB models with early stopping.
     Args:
@@ -27,14 +29,17 @@ def train_model(model, full_dataset, opt_params, model_type="dsib", device=devic
     Returns:
         A tuple (train_estimates, test_estimates) containing mutual information estimates.
     """
+    # Initialize model
+    model_name = model_func.__name__
+    model = model_func(critic_params)
     model.to(device)  # Ensure model is on GPU
+    # Pull out data loaders
+    train_data, (test_X, test_Y), (eval_X, eval_Y) = full_dataset
+    # Initialize variables
     epochs = opt_params['epochs']
     opt = torch.optim.Adam(model.parameters(), lr=opt_params['learning_rate'])
     estimates_mi_train = []
     estimates_mi_test = []
-    # Pull out data loaders
-    train_data, (test_X, test_Y), (eval_X, eval_Y) = full_dataset
-    # Early stopping variables
     best_estimator_ts = float('-inf')  # Initialize with negative infinity
     no_improvement_count = 0
 
@@ -42,36 +47,34 @@ def train_model(model, full_dataset, opt_params, model_type="dsib", device=devic
         for i, (x, y) in enumerate(iter(train_data)):
             # Squeeze to remove batch dim. BatchedDataset handles batches, so it's always just 1
             x, y = x.squeeze(dim=0).T.to(device), y.squeeze(dim=0).T.to(device)
-            if epoch == 0:
-                print(x)
             opt.zero_grad()
             # Compute loss based on model type
-            if model_type == "dsib":
+            if model_name == "DSIB":
                 loss = model(x, y)  # DSIB returns a single loss
-            elif model_type == "dvsib":
+            elif model_name == "DVSIB":
                 loss, _, _ = model(x, y)  # DVSIB returns three outputs
             else:
-                raise ValueError("Invalid model_type. Choose 'dsib' or 'dvsib'.")
+                raise ValueError("Invalid model_type. Choose 'DSIB' or 'DVSIB'.")
             loss.backward()
             opt.step()
         # Evaluate the model at every epoch
         with torch.no_grad():
-            if model_type == "dsib":
+            if model_name == "DSIB":
                 estimator_tr = -model(eval_X, eval_Y)
                 estimator_ts = -model(test_X, test_Y)
-            elif model_type == "dvsib": # Get lossGout, that is the mi value
+            elif model_name == "DVSIB": # Get lossGout, that is the mi value
                 _, _, estimator_tr = model(eval_X, eval_Y)
                 _, _, estimator_ts = model(test_X, test_Y)
             estimator_tr = estimator_tr.to('cpu').detach().numpy()
             estimator_ts = estimator_ts.to('cpu').detach().numpy()
             estimates_mi_train.append(estimator_tr)
             estimates_mi_test.append(estimator_ts)
-        print(f"Epoch: {epoch+1}, {model_type}, train: {estimator_tr}, test: {estimator_ts}", flush=True)
+        print(f"Epoch: {epoch+1}, {model_name}, train: {estimator_tr}, test: {estimator_ts}", flush=True)
         
         # Check for improvement or negative values
         if estimator_ts < 0:
             no_improvement_count += 1
-        elif estimator_ts > best_estimator_ts + min_delta:
+        elif estimator_ts > best_estimator_ts + opt_params['min_delta']:
             # We have an improvement
             best_estimator_ts = estimator_ts
             no_improvement_count = 0
@@ -79,12 +82,42 @@ def train_model(model, full_dataset, opt_params, model_type="dsib", device=devic
             # No significant improvement
             no_improvement_count += 1
         # Check if we should stop early
-        if no_improvement_count >= patience:
+        if no_improvement_count >= opt_params['patience']:
             print(f"Early stopping triggered after {epoch+1} epochs. Best estimator_ts: {best_estimator_ts}")
             break
     
     return np.array(estimates_mi_train), np.array(estimates_mi_test)
 
+
+
+def run_trial(j, model_func, input_queue, results_queue, opt_params, critic_params, device):
+    """
+    Run a single trial j (and possibly other conditions in opt_params, critic_params, etc)
+    
+    Parameters:
+        j (int): Trial index.
+        input_queue: multiprocessing queue to retrieve dataset from
+        results_queue: multiprocessing queue to send result MI values to
+        # dataset (BatchedDataset): Dataset of neuron + muscle data
+        opt_params (dict): Optimization and run parameters.
+        critic_params (dict): Critic parameters. Assumes it includes field 'embed_dim': dz
+        estimators (list): List of estimators.
+        model_types (list): List of model types.
+        device: Device to use (e.g., 'cuda', 'mps', 'cpu').
+    Returns:
+        tuple: Results for this trial (key, mis, mis_test).
+    """
+    # Set device explicitly for this process (?)
+    # torch.cuda.empty_cache()  # Clear cache before starting
+    torch.device(device)
+    # Get dataset from queue, create dataloader for train, split out eval and test
+    dataset = input_queue.get()
+    full_dataset = create_train_test_eval(dataset, train_fraction=0.95, device=device)
+    # Initialize and train the model
+    mis, mis_test = train_model(model_func, full_dataset, opt_params, critic_params)
+    # Return results as a tuple
+    results_queue.put((f"trial_{j}_dz_{critic_params['embed_dim']}", mis, mis_test))
+    # return f"trial_{j}_dz_{dz}", mis, mis_test
 
 def train_model_parallel(full_dataset, model_func, critic_params, opt_params, device=device, patience=50, min_delta=0.001):
     """
@@ -118,8 +151,6 @@ def train_model_parallel(full_dataset, model_func, critic_params, opt_params, de
         for i, (x, y) in enumerate(iter(train_data)):
             # Squeeze to remove batch dim. BatchedDataset handles batches, so it's always just 1
             x, y = x.squeeze(dim=0).T.to(device), y.squeeze(dim=0).T.to(device)
-            if epoch == 0:
-                print(x)
             opt.zero_grad()
             # Compute loss based on model type
             if model_name == "DSIB":
@@ -132,6 +163,8 @@ def train_model_parallel(full_dataset, model_func, critic_params, opt_params, de
             opt.step()
         # Evaluate the model at every epoch
         with torch.no_grad():
+            if epoch == 0:
+                print(torch.any(eval_X != 0))
             if model_name == "DSIB":
                 estimator_tr = -model(eval_X, eval_Y)
                 estimator_ts = -model(test_X, test_Y)
