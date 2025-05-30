a31dbb04eb30c0938b469fa8f2bc82bc1f34883c
Wood <lwood39@gatech.edu>
2025-05-30 12:51:16 -0400

    Added dvsib variant of cnn, removed chunking code that didn't work

--- eslam_MI/models.py
+++ eslam_MI/models.py
@@ -57,13 +57,7 @@ class DSIB(nn.Module):
             if self.params['mode'] == "bi":
                 # Get the rotated version ready 
                 zX = self.bilinear(zX)
-            # Chunked vs regular version
-            if (self.params['chunked_inference'] and
-                (self.params['chunk_size'] < batch_size) and 
-                (self.params['estimator'] == 'infonce')):
-                mi = estimate_full_mutual_information_infonce(self, zX, zY, chunk_size=self.params['chunk_size'])
-            else:
-                mi = self.info(zX, zY, batch_size)
+            mi = self.info(zX, zY, batch_size)
             lossGout = mi
         elif self.params['mode'] == "concat":
             x_expanded = dataX.repeat(batch_size, 1)  # Repeat batch_size times
@@ -107,20 +101,6 @@ class DVSIB(nn.Module):
 
     def forward(self, dataX, dataY):
         batch_size = dataX.shape[0]  # Dynamically infer batch size
-        # Chunked version of sep infonce inference, if asked
-        if (self.params['chunked_inference'] and
-            (self.params['mode'] == 'sep') and 
-            (self.params['chunk_size'] < batch_size) and 
-            (self.params['estimator'] == 'infonce')):
-            # Pre-compute all embeddings before running estimate in chunks
-            with torch.no_grad():
-                _, _, zX = self.encoder_x(dataX)
-                _, _, zY = self.encoder_y(dataY)
-            lossGin = self.encoder_x.kl_loss + self.encoder_y.kl_loss
-            lossGout = estimate_full_mutual_information_infonce(self, zX, zY, chunk_size=self.params['chunk_size'])
-            loss = lossGin - self.params['beta']*lossGout
-            return [loss, lossGin, lossGout]
-
         if self.params['mode'] in ["sep", "bi"]:
             _, _, zX = self.encoder_x(dataX) # Samples
             _, _, zY = self.encoder_y(dataY)
@@ -142,3 +122,52 @@ class DVSIB(nn.Module):
         return [loss, lossGin, lossGout]
 
 
+class conv_DVSIB(nn.Module):
+    def __init__(self, estimator, params, mode="sep", baseline_fn=None):
+        super(conv_DVSIB, self).__init__()
+        self.mode = mode
+        self.params = params
+        if mode in ["sep", "bi"]:
+            # Create two encoders from |X/Y| to |Z_X/Y|
+            self.encoder_x = var_cnn_mlp(
+                input_channels=params['input_channels_x'], hidden_dim=params['hidden_dim_x'], output_dim=params['embed_dim'],
+                conv_layers=params['conv_layers_x'], fc_layers=params['fc_layers_x'], activation=params['activation_x']
+            )
+            self.encoder_y = var_cnn_mlp(
+                input_channels=params['input_channels_y'], hidden_dim=params['hidden_dim_y'], output_dim=params['embed_dim'],
+                conv_layers=params['conv_layers_y'], fc_layers=params['fc_layers_y'], activation=params['activation_y']
+            )
+        if mode == "bi":
+            # Add additional layer to the separable if bilinear
+            self.bilinear = nn.Linear(params['embed_dim'], params['embed_dim'], bias=False)
+        if mode == "concat":
+            # Create one encoder that takes |X|+|Y| and outputs 1, it has max the width as the separable/bilinear
+            self.encoder_xy = var_cnn_mlp(
+                input_channels=params['input_channels_x'] + params['input_channels_y'], hidden_dim=max(params['hidden_dim_x'],params['hidden_dim_y']), output_dim=1,
+                conv_layers=max(params['conv_layers_x'],params['conv_layers_y']), fc_layers=max(params['fc_layers_x'],params['fc_layers_y']), activation=params['activation_x']
+            ) # use x activation
+        # Estimator
+        self.info = decoder_INFO(estimator, mode, baseline_fn)
+
+    def forward(self, dataX, dataY):
+        batch_size = dataX.shape[0]  # Dynamically infer batch size
+        if self.mode in ["sep", "bi"]:
+            _,_,zX = self.encoder_x(dataX) # Samples
+            _,_,zY = self.encoder_y(dataY)
+            if self.mode == "bi":
+                # Get the rotated version ready 
+                zX = self.bilinear(zX)
+            lossGin=self.encoder_x.kl_loss + self.encoder_y.kl_loss
+            mi = self.info(zX, zY, batch_size)
+            lossGout = mi
+        elif self.mode == "concat":
+            x_expanded = dataX.repeat(batch_size, 1)  # Repeat batch_size times
+            y_expanded = dataY.repeat_interleave(batch_size, dim=0)  # Repeat within batch
+            xy_pairs = torch.cat((x_expanded, y_expanded), dim=1)  # Concatenate along feature dim
+            _, _, scores = self.encoder_xy(xy_pairs)
+            lossGin=self.encoder_xy.kl_loss
+            mi = self.info(scores, None, batch_size)  # scores is in the place of zX
+            lossGout = mi
+        
+        loss = lossGin - self.params['beta']*lossGout
+        return [loss, lossGin, lossGout]
