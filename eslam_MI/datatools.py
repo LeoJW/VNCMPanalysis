fixed index clipping bug in precision ground truth

eslam_MI/datatools.py
@@ -121,8 +121,7 @@ class BatchedDatasetWithNoise(Dataset):
         self.X.zero_()
         self.new_indices = torch.clip(
             self.spike_indices[3] + torch.rand(self.spike_indices[3].shape, device=device).mul_(amplitude).round_().int(), 
-            0, 
-            self.X.shape[3] - 1)
+            0, self.X.shape[3] - 1)
         self.X[self.spike_indices[0], self.spike_indices[1], self.spike_indices[2], self.new_indices] = 1
     def apply_noise_Y(self, amplitude):
         """
@@ -132,8 +131,7 @@ class BatchedDatasetWithNoise(Dataset):
         self.Y.zero_()
         self.new_indices = torch.clip(
             self.spike_indices_Y[3] + torch.rand(self.spike_indices_Y[3].shape, device=device).mul_(amplitude).round_().int(), 
-            0, 
-            self.Y.shape[3] - 1)
+            0, self.Y.shape[3] - 1)
         self.Y[self.spike_indices_Y[0], self.spike_indices_Y[1], self.spike_indices_Y[2], self.new_indices] = 1
     def time_lag(self, lag, channels=None):
         """
@@ -185,7 +183,8 @@ def create_data_split(dataset, batch_size, train_fraction=0.95, eval_fraction=No
 
 
 # Read neuron and muscle data for one moth, return X and Y with specific binning
-def read_spike_data(base_name, bin_size=None, neuron_label_filter=None, sample_rate=30000, set_precision=0):
+def read_spike_data(base_name, bin_size=None, neuron_label_filter=None, sample_rate=30000, 
+    set_precision_x=0, set_precision_y=0):
     """
     Processes spike data from 3 .npz files into neural (X) and muscle (Y) activity tensors.
     Returns bool, always binarizes. If a bin has multiple spikes occur, will just appear as 1
@@ -254,9 +253,9 @@ def read_spike_data(base_name, bin_size=None, neuron_label_filter=None, sample_r
         else:
             indices = np.rint(data[unit] / scale)
         # Set to precision level if requested
-        if set_precision != 0:
-            prec_samples = set_precision / bin_size
-            indices = np.rint(np.rint(indices / prec_samples) * prec_samples)
+        if set_precision_x != 0:
+            prec_samples = set_precision_x / bin_size
+            indices = np.clip(np.rint(np.rint(indices / prec_samples) * prec_samples), 0, X.shape[1])
         X[i, indices] = 1
     # Create binary spike trains for muscles
     for i, unit in enumerate(muscle_labels):
@@ -264,6 +263,9 @@ def read_spike_data(base_name, bin_size=None, neuron_label_filter=None, sample_r
             indices = data[unit]
         else:
             indices = np.rint(data[unit] / scale)
+        if set_precision_y != 0:
+            prec_samples = set_precision_y / bin_size
+            indices = np.clip(np.rint(np.rint(indices / prec_samples) * prec_samples), 0, X.shape[1])
         Y[i, indices] = 1
     # Trim to only bouts
     indices = torch.concat([torch.arange(s,e, dtype=int) for s,e in zip(starts, ends)])