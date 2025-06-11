fixed checking activity bug, was not off-by-one

eslam_MI/datatools.py
@@ -13,7 +13,6 @@ else:
     device = "cpu"
 
 
-
 class BatchedDataset(Dataset):
     """
     Dataset that supports both batch-wise and full data access.
@@ -140,21 +139,23 @@ class BatchedDatasetWithNoise(Dataset):
             self.X[i,0,:,0:ind_diff] = self.Xmain[:, indices[0]:indices[1]]
             self.Y[i,0,:,0:ind_diff] = self.Ymain[:, indices[0]:indices[1]]
         # Get spike indices
-        self.spike_indices = torch.where(self.X)
-        self.spike_indices_Y = torch.where(self.Y)
+        self.spike_indices = torch.nonzero(self.X, as_tuple=False)
+        self.spike_indices_Y = torch.nonzero(self.Y, as_tuple=False)
         # If checking activity, remove chunks where there is no activity
         if check_activity:
             # Find batches with activity in both
-            keep_batches, validX, validY = np.intersect1d(self.spike_indices[0].cpu(), self.spike_indices_Y[0].cpu(), return_indices=True)
+            keep_batches, validX, validY = np.intersect1d(self.spike_indices[:,0].cpu(), self.spike_indices_Y[:,0].cpu(), return_indices=True)
             # Update everything
             self.X = self.X[keep_batches,:,:,:]
             self.Y = self.Y[keep_batches,:,:,:]
             self.n_batches = len(keep_batches)
-            self.spike_indices = tuple(tensor[validX] for tensor in self.spike_indices)
-            self.spike_indices_Y = tuple(tensor[validY] for tensor in self.spike_indices_Y)
+            self.spike_indices = self.spike_indices[validX,:]
+            self.spike_indices_Y = self.spike_indices_Y[validY,:]
+            self.spike_indices[:,0] = torch.searchsorted(torch.tensor(keep_batches, device=device), self.spike_indices[:,0].contiguous())
+            self.spike_indices_Y[:,0] = torch.searchsorted(torch.tensor(keep_batches, device=device), self.spike_indices_Y[:,0].contiguous())
             self.batch_indices = [self.batch_indices[i] for i in keep_batches]
         # Intermediate tensor for holding noise indices
-        self.new_indices = torch.zeros_like(self.spike_indices[3], dtype=int, device=device)
+        self.new_indices = torch.zeros_like(self.spike_indices[:,3], dtype=int, device=device)
     def __len__(self):
         return self.n_batches
     def __getitem__(self, idx):
@@ -170,9 +171,9 @@ class BatchedDatasetWithNoise(Dataset):
         """
         self.X.zero_()
         self.new_indices = torch.clip(
-            self.spike_indices[3] + torch.rand(self.spike_indices[3].shape, device=device).mul_(amplitude).round_().int(), 
+            self.spike_indices[:,3] + torch.rand(self.spike_indices[:,3].shape, device=device).mul_(amplitude).round_().int(), 
             0, self.X.shape[3] - 1)
-        self.X[self.spike_indices[0], self.spike_indices[1], self.spike_indices[2], self.new_indices] = 1
+        self.X[self.spike_indices[:,0], self.spike_indices[:,1], self.spike_indices[:,2], self.new_indices] = 1
     def apply_noise_Y(self, amplitude):
         """
         Apply noise to spike times of noisy version of Y. 
@@ -180,9 +181,9 @@ class BatchedDatasetWithNoise(Dataset):
         """
         self.Y.zero_()
         self.new_indices = torch.clip(
-            self.spike_indices_Y[3] + torch.rand(self.spike_indices_Y[3].shape, device=device).mul_(amplitude).round_().int(), 
+            self.spike_indices_Y[:,3] + torch.rand(self.spike_indices_Y[:,3].shape, device=device).mul_(amplitude).round_().int(), 
             0, self.Y.shape[3] - 1)
-        self.Y[self.spike_indices_Y[0], self.spike_indices_Y[1], self.spike_indices_Y[2], self.new_indices] = 1
+        self.Y[self.spike_indices_Y[:,0], self.spike_indices_Y[:,1], self.spike_indices_Y[:,2], self.new_indices] = 1
     def time_lag(self, lag, channels=None):
         """
         Apply time lag to spike times of all (or specific) neurons/muscles 

eslam_MI/models.py
@@ -11,7 +11,7 @@ if torch.cuda.is_available():
 elif torch.backends.mps.is_available():
     device = 'mps'
 else:
-    device = "cpu"
+    device = 'cpu'
 
 class DSIB(nn.Module):
     def __init__(self, params, baseline_fn=None):
@@ -50,7 +50,7 @@ class DSIB(nn.Module):
             zX = torch.zeros(batch_size, self.params['embed_dim'], device=dataX.device)
             zY = torch.zeros(batch_size, self.params['embed_dim'], device=dataX.device)
             for i in range(0, batch_size, self.params['batch_size']):
-                end_idx = min(i + self.params['batch_size'], batch_size - 1)
+                end_idx = min(i + self.params['batch_size'], batch_size)
                 zX[i:end_idx,:] = self.encoder_x(dataX[i:end_idx,:,:,:])
                 zY[i:end_idx,:] = self.encoder_y(dataY[i:end_idx,:,:,:])
         else:

eslam_MI/run_binning_comparison.py
@@ -58,7 +58,7 @@ elif torch.backends.mps.is_available():
     synchronize = torch.mps.synchronize
     empty_cache = torch.mps.empty_cache
 else:
-    device = "CPU"
+    device = 'cpu'
     synchronize = lambda: None
     empty_cache = lambda: None
 print(f'Device: {device}')

eslam_MI/trainers.py
@@ -18,7 +18,7 @@ if torch.cuda.is_available():
 elif torch.backends.mps.is_available():
     device = 'mps'
 else:
-    device = "cpu"
+    device = 'cpu'
 
 # Train functions specific to CNN model architecture
 def train_cnn_model(full_dataset, params, device=device, subset_indices=None):

eslam_MI/utils.py
@@ -246,12 +246,12 @@ class MultiScaleConvBlock(nn.Module):
             padding_b2 = dilation
             dilation = dilation
         # Calculate channels for each branch (distribute output channels more flexibly)
-        # Strategy: Give larger branches slightly more channels
+        # Gives larger branches slightly more channels if not divisible by 2
         base_channels = out_channels // 2
         remainder = out_channels % 2
         # Distribute remainder channels to branches (prioritize faster branches)
         branch1_channels = base_channels + remainder  # 3x3 conv  
-        branch2_channels = base_channels + remainder  # 5x5 conv
+        branch2_channels = base_channels + remainder  # 3x3 conv dilated
         # Branch 1: 3x3 conv (standard receptive field)
         self.branch1 = nn.Sequential(
             nn.Conv2d(in_channels, branch1_channels, kernel_size=kernel_size, stride=stride, padding=padding_b1, bias=False),