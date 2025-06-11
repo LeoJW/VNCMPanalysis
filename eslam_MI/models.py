5e60aa3c9c503740c4e2f152f65bb05f6c91ca86
LeoJW <leo@woodtx.com>
2025-06-11 16:28:47 -0400

    off-by-one error fixed in DSIB

--- eslam_MI/models.py
+++ eslam_MI/models.py
@@ -50,7 +50,7 @@ class DSIB(nn.Module):
             zX = torch.zeros(batch_size, self.params['embed_dim'], device=dataX.device)
             zY = torch.zeros(batch_size, self.params['embed_dim'], device=dataX.device)
             for i in range(0, batch_size, self.params['batch_size']):
-                end_idx = min(i + self.params['batch_size'], batch_size)
+                end_idx = min(i + self.params['batch_size'], batch_size - 1)
                 zX[i:end_idx,:] = self.encoder_x(dataX[i:end_idx,:,:,:])
                 zY[i:end_idx,:] = self.encoder_y(dataY[i:end_idx,:,:,:])
         else:
